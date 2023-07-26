65c74f9aab211ac217450b099a60fe6ab8c7152d
Darren Pavel <darren.pavel@gmail.com>
2023-07-26 13:03:45 -0700

    Create empty stand-ins for controllers Add store to client/index.js

--- client/index.js
+++ client/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './components/App.jsx';
-import { BrowserRouter } from 'react-router-dom';
+import store from './store';
 
 const root = createRoot(document.querySelector('#root'));
 root.render(
